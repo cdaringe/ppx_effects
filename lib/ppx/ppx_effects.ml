a88c745eeaaee28a1d39aa69702ac06073b61026
cdaringe <cdaringe@gmail.com>
2021-10-12 23:20:19 -0700

    wip

--- lib/ppx/ppx_effects.ml
+++ lib/ppx/ppx_effects.ml
@@ -2,7 +2,24 @@
 open Ppxlib
 open Ast_builder.Default
 
-let extension =
+(* type z = Parsetree.t
+   type l = Longident.t *)
+
+(* let expr_to_core = function
+   | Ptyp_any -> ptyp_any
+   | Ptyp_var v -> ptyp_var v
+   | Ptyp_arrow (a,b,c) -> ptyp_arrow a b c
+   | Ptyp_tuple ctl -> ptyp_tuple ctl
+   | Ptyp_constr (a,b) -> ptyp_constr a b
+   | Ptyp_object (object_field_list, closed_flag) -> ptyp_object object_field_list closed_flag
+   | Ptyp_class (lloc, ctl) -> ptyp_class lloc ctl
+   | Ptyp_alias (ct, s) -> ptyp_alias ct s
+   | Ptyp_variant (a,b,c) -> ptyp_variant a b c
+   | Ptyp_poly (sll, ct) -> ptyp_poly sll ct
+   | Ptyp_package pt -> ptyp_package pt
+   | Ptyp_extension extension -> ptyp_extension extension *)
+
+let effect_keyword_extension =
   Extension.declare "effect" Ppxlib.Extension.Context.Structure_item
     Ast_pattern.(
       pstr
@@ -36,6 +53,88 @@ let extension =
              ]
            ~private_:Public)
 
-let rule = Context_free.Rule.extension extension
+let loca ~loc v = Loc.make ~loc v
 
-let () = Driver.register_transformation ~rules:[ rule ] "effect"
+let try_with_ident ~loc =
+  loca ~loc @@ Ppxlib.Longident.parse "Obj.Effect_handlers.Deep.try_with"
+
+let continue_ident ~loc =
+  loca ~loc @@ Ppxlib.Longident.parse "Obj.Effect_handlers.Deep.continue"
+
+let var_try_with_private ~loc =
+  ppat_var ~loc (loca ~loc "__ppx_effects_try_with")
+
+let var_continue_private ~loc =
+  ppat_var ~loc (loca ~loc "__ppx_effects_continue")
+
+let assign_continue ~loc expr =
+  pexp_let ~loc Nonrecursive
+    [
+      value_binding ~loc
+        ~pat:(var_continue_private ~loc)
+        ~expr:(pexp_ident ~loc (continue_ident ~loc));
+    ]
+    expr
+
+let assign_try_with ~loc expr =
+  pexp_let ~loc Nonrecursive
+    [
+      value_binding ~loc
+        ~pat:(var_try_with_private ~loc)
+        ~expr:(pexp_ident ~loc (try_with_ident ~loc));
+    ]
+    expr
+
+let with_effects_refs ~loc expr =
+  expr |> assign_continue ~loc |> assign_try_with ~loc
+
+let invoke_try_with_effects ~loc ~(comp) ~arg ~handler_record =
+  let try_with = pexp_ident ~loc (try_with_ident ~loc) in
+  with_effects_refs ~loc
+  @@ pexp_apply ~loc try_with
+       [ (Nolabel, comp); (Nolabel, arg); (Nolabel, handler_record) ]
+
+let to_handler_record_kv ~loc handler = 1
+let non_with_record = None
+let to_effc_handler_record ~loc handlers = pexp_record ~loc (List.map (to_handler_record_kv ~loc) handlers) non_with_record
+
+let extract_eff_and_handler = function
+  | {
+      pexp_desc = Parsetree.Pexp_construct (
+        {txt = effect_name; _; },
+        Some {
+          pexp_desc = Pexp_ident {txt = Lident handler; _ };
+          _; 
+        }
+      );
+      _;
+   } -> effect_name, handler
+  | _ -> failwith "invalid hanler entry"
+let with_effects_keyword_extension =
+  Extension.declare "with_effects" Ppxlib.Extension.Context.Expression
+    Ast_pattern.(pstr (pstr_eval (pexp_apply (pexp_ident __) __) nil ^:: nil))
+    (fun ~loc ~path:_ comp params ->
+      let (arg, handlers) = match params with
+        | [(Nolabel, {
+          pexp_desc = Pexp_construct (arg, None);
+          _
+          });(Nolabel, {pexp_desc =
+          Pexp_array handler_pexp_constructs; _ })] -> (arg, List.map extract_eff_and_handler handler_pexp_constructs)
+        | _ -> failwith "supported args: <how to pretty print AST programatically?>" (* Ppxlib_ast.Pprintast.expression *)   in
+      let open_in_effects_handlers_deep expr =
+        let ed_mod_ident =
+          pmod_ident ~loc (loca ~loc @@ lident "Obj.Effect_handlers.Deep")
+        in
+        pexp_open ~loc (open_infos ~loc ~expr:ed_mod_ident ~override:Fresh) expr
+      in
+      invoke_try_with_effects ~loc ~comp ~arg ~handler_record:(to_effc_handler_record ~loc handlers)
+
+let effect_rule = Context_free.Rule.extension effect_keyword_extension
+
+let with_effects_rules =
+  Context_free.Rule.extension with_effects_keyword_extension
+
+let () =
+  Driver.register_transformation
+    ~rules:[ effect_rule; with_effects_rules ]
+    "effect"
--- test/ppx.ml/test.ml
+++ test/ppx.ml/test.ml
@@ -1,3 +1,15 @@
 open Obj.Effect_handlers
 
 [%%effect E1, string, unit]
+
+[%%effect E2, unit, unit]
+
+[%%effect E3, string, string]
+
+let comp () = ()
+
+let fn () =
+  let handle_e1 _unused_string continue k = continue k () in
+  let handle_e2 _ continue k = continue k () in
+  let handle_e3 s continue k = continue k s in
+  [%with_effects comp () [| E1 handle_e1; E2 handle_e2; E3 handle_e3 |]]
