e393e45d8e8c1413da1faa672160c0fb3f5e662b
cdaringe <cdaringe@gmail.com>
2021-10-14 18:23:13 -0700

    feat: drop old impl, correct tests, add ci

--- lib/ppx/ppx_effects.ml
+++ lib/ppx/ppx_effects.ml
@@ -16,7 +16,10 @@ let effect_keyword_extension =
       let eff_name =
         match effect_construct with
         | Lident z -> z
-        | _ -> failwith "you shouldn't be using this"
+        | _ ->
+            failwith
+              "effect extension is WIP. send a patch :) only simple types \
+               supported"
       in
       let effsym = ptyp_constr ~loc (Loc.make ~loc (Lident "eff")) in
       pstr_typext ~loc
@@ -54,107 +57,13 @@ let invoke_try_with_effects ~loc ~comp ~arg ~handler_record =
   pexp_apply ~loc try_with
     [ (Nolabel, comp); (Nolabel, arg); (Nolabel, handler_record) ]
 
-let some ~loc expr = pexp_construct ~loc (loca ~loc (lident "Some")) expr
-
-let none ~loc = pexp_construct ~loc (loca ~loc (lident "None")) None
-
 let lident_l_of_str ~loc v = loca ~loc (Longident.Lident v)
-
-(*
-Convert the ppx syntax: 
-  (E1 handle_e1) 
-
-Into:
-  E1 x -> Some (fun (k : (a, _) continuation) -> handle_e1 x continue k) 
-  *)
-let to_effect_handler_match_case ~loc (handler : longident * label) =
-  let eff_constructor_lident, handler_name = handler in
-  let lhs_ident = loca ~loc eff_constructor_lident in
-  let lhs =
-    ppat_construct ~loc lhs_ident (Some (ppat_var ~loc (loca ~loc "x")))
-  in
-  (* (k : (a, _) *)
-  let a_constr = ptyp_constr ~loc (lident_l_of_str ~loc "a") [] in
-  let rhs_constraint =
-    ppat_constraint ~loc
-      (ppat_var ~loc (loca ~loc "k"))
-      (ptyp_constr ~loc (continuation_ident ~loc) [ a_constr; ptyp_any ~loc ])
-  in
-  let rhs_apply_fn =
-    pexp_apply ~loc
-      (pexp_ident ~loc @@ lident_l_of_str ~loc handler_name)
-      [
-        (Nolabel, pexp_ident ~loc (lident_l_of_str ~loc "x"));
-        (Nolabel, pexp_ident ~loc (continue_ident ~loc));
-        (Nolabel, pexp_ident ~loc (lident_l_of_str ~loc "k"));
-      ]
-  in
-  let rhs_lambda = pexp_fun ~loc Nolabel None rhs_constraint rhs_apply_fn in
-  let rhs = some ~loc @@ Some rhs_lambda in
-  case ~lhs ~guard:None ~rhs
-
-let case_match_none ~loc =
-  case ~lhs:(ppat_any ~loc) ~guard:None ~rhs:(none ~loc)
-
-let simple_record = None
-
-type handler_list = (longident * label) list
-
-let to_effc_handler_record ~loc (handlers : handler_list) =
-  let match_effect_cases =
-    List.map (to_effect_handler_match_case ~loc) handlers
-    @ [ case_match_none ~loc ]
-  in
-  let match_arg = pexp_ident ~loc @@ lident_l_of_str ~loc "e" in
-  let match_effects = pexp_match ~loc match_arg match_effect_cases in
-  let effc_fn =
-    pexp_newtype ~loc (loca ~loc "a")
-    @@ pexp_fun ~loc Nolabel None
-         (ppat_constraint ~loc
-            (ppat_var ~loc (loca ~loc "e"))
-            (ptyp_constr ~loc
-               (lident_l_of_str ~loc "eff")
-               [ ptyp_constr ~loc (lident_l_of_str ~loc "a") [] ]))
-         match_effects
-  in
-  let effc_entry = (lident_l_of_str ~loc "effc", effc_fn) in
-  pexp_record ~loc [ effc_entry ] simple_record
-
-let extract_eff_and_handler = function
-  | {
-      pexp_desc =
-        Parsetree.Pexp_construct
-          ( { txt = effect_name; _ },
-            Some { pexp_desc = Pexp_ident { txt = Lident handler_name; _ }; _ }
-          );
-      _;
-    } ->
-      (effect_name, handler_name)
-  | _ -> failwith "invalid hanler entry"
-
-let with_effects_keyword_extension =
-  Extension.declare "with_effects" Ppxlib.Extension.Context.Expression
-    Ast_pattern.(pstr (pstr_eval (pexp_apply __ __) nil ^:: nil))
-    (fun ~loc ~path:_ comp params ->
-      let arg, handlers =
-        match params with
-        | [
-         (Nolabel, arg);
-         (Nolabel, { pexp_desc = Pexp_array handler_pexp_constructs; _ });
-        ] ->
-            (arg, List.map extract_eff_and_handler handler_pexp_constructs)
-        | _ ->
-          Location.raise_errorf ~loc "Unsupported expression %a"
-            (Pprintast.expression)
-            (Ppxlib.Ast_builder.Default.pexp_apply ~loc comp params)
-      in
-      invoke_try_with_effects ~loc ~comp ~arg
-        ~handler_record:(to_effc_handler_record ~loc handlers))
 
 let list_split_last h tl =
   let rec loop last acc = function
-    | [] -> last, List.rev acc
-    | h :: t -> loop h (last :: acc) t in
+    | [] -> (last, List.rev acc)
+    | h :: t -> loop h (last :: acc) t
+  in
   loop h [] tl
 
 (*
@@ -164,51 +73,68 @@ let list_split_last h tl =
      | (E (...) : a eff) -> Some (fun (k : (a, _) continuation) -> exp)
 *)
 let convert_case_to_effect_handler ~loc = function
-  | { pc_lhs={
-    ppat_desc=Ppat_construct (name, Some({ppat_desc=Ppat_tuple (p_h :: p_tl);_} as args));
-    _ } as pc_lhs;
-    pc_guard; pc_rhs } ->
-    let a_constr = ptyp_constr ~loc (lident_l_of_str ~loc "a") [] in
-    let kont_pat, pats = list_split_last p_h p_tl in
-    let kont_pat =
-      Ast_builder.Default.ppat_constraint ~loc kont_pat
-      (ptyp_constr ~loc (continuation_ident ~loc) [ a_constr; ptyp_any ~loc ])
-    in
-    let args = match pats with
-      | [pat] -> pat
-      | _ -> {args with ppat_desc = Ppat_tuple pats} in
-    let pc_lhs = [%pat? ([%p {pc_lhs with ppat_desc = Ppat_construct (name, Some args)}] : a eff)] in
-    let pc_rhs = [%expr Some (fun [%p kont_pat] -> [%e pc_rhs])] in
-    {pc_lhs; pc_guard; pc_rhs}
+  | {
+      pc_lhs =
+        {
+          ppat_desc =
+            Ppat_construct
+              (name, Some ({ ppat_desc = Ppat_tuple (p_h :: p_tl); _ } as args));
+          _;
+        } as pc_lhs;
+      pc_guard;
+      pc_rhs;
+    } ->
+      let a_constr = ptyp_constr ~loc (lident_l_of_str ~loc "a") [] in
+      let kont_pat, pats = list_split_last p_h p_tl in
+      let kont_pat =
+        Ast_builder.Default.ppat_constraint ~loc kont_pat
+          (ptyp_constr ~loc (continuation_ident ~loc)
+             [ a_constr; ptyp_any ~loc ])
+      in
+      let args =
+        match pats with
+        | [ pat ] -> pat
+        | _ -> { args with ppat_desc = Ppat_tuple pats }
+      in
+      let pc_lhs =
+        [%pat?
+          ([%p { pc_lhs with ppat_desc = Ppat_construct (name, Some args) }] :
+            a eff)]
+      in
+      let pc_rhs = [%expr Some (fun [%p kont_pat] -> [%e pc_rhs])] in
+      { pc_lhs; pc_guard; pc_rhs }
   | v ->
-    Location.raise_errorf ~loc "Use of unsupported pattern %a" Pprintast.pattern v.pc_lhs
+      Location.raise_errorf ~loc "Use of unsupported pattern %a"
+        Pprintast.pattern v.pc_lhs
 
 let effect_try_extension =
   Extension.declare "effect" Ppxlib.Extension.Context.Expression
-    Ast_pattern.(pstr (pstr_eval (pexp_try __ __) nil ^:: nil)) begin
-    fun ~loc ~path:_ comp cases ->
-      let updated_cases = List.map (convert_case_to_effect_handler ~loc) cases in
-      let default_case = Ast_builder.Default.(case ~lhs:(ppat_any ~loc) ~guard:None ~rhs:[%expr None]) in
-      let cases = updated_cases @ [default_case] in
+    Ast_pattern.(pstr (pstr_eval (pexp_try __ __) nil ^:: nil))
+    (fun ~loc ~path:_ comp cases ->
+      let updated_cases =
+        List.map (convert_case_to_effect_handler ~loc) cases
+      in
+      let default_case =
+        Ast_builder.Default.(
+          case ~lhs:(ppat_any ~loc) ~guard:None ~rhs:[%expr None])
+      in
+      let cases = updated_cases @ [ default_case ] in
       let comp = [%expr fun () -> [%e comp]] in
       let arg = [%expr ()] in
 
       let effect_handler =
-        let a_ident = ({txt="a";loc})  in
+        let a_ident = { txt = "a"; loc } in
         Ast_builder.Default.(pexp_newtype ~loc a_ident)
-        (Ast_builder.Default.pexp_function ~loc cases)
+          (Ast_builder.Default.pexp_function ~loc cases)
       in
-      invoke_try_with_effects
-        ~loc ~comp ~arg ~handler_record:([%expr { effc = [%e effect_handler] } ])
-  end
+      invoke_try_with_effects ~loc ~comp ~arg
+        ~handler_record:[%expr { effc = [%e effect_handler] }])
 
 let effect_rule = Context_free.Rule.extension effect_keyword_extension
 
-let with_effects_rules =
-  [Context_free.Rule.extension with_effects_keyword_extension;
-   Context_free.Rule.extension effect_try_extension ]
+let try_effects_rule = Context_free.Rule.extension effect_try_extension
 
 let () =
   Driver.register_transformation
-    ~rules:( effect_rule :: with_effects_rules )
+    ~rules:[ effect_rule; try_effects_rule ]
     "effects"
--- test/test.ml
+++ test/test.ml
@@ -13,22 +13,6 @@ let comp on_complete =
   on_complete ()
 
 let () =
-  let handle_a s continue k =
-    print_endline @@ "handling a with: " ^ s;
-    continue k ()
-  in
-  let handle_b _ continue k =
-    print_endline "handling b";
-    continue k ()
-  in
-  let handle_c s continue k =
-    print_endline @@ "handling c with: " ^ s;
-    continue k "c_output"
-  in
-  let on_complete () = print_endline "all_done!" in
-  [%with_effects comp on_complete [| A handle_a; B handle_b; C handle_c |]]
-  
-let () =
   let handle_a s k =
     print_endline @@ "handling a with: " ^ s;
     Obj.Effect_handlers.Deep.continue k ()
@@ -45,4 +29,4 @@ let () =
   try%effect comp on_complete with
   | A (s, k) -> handle_a s k
   | B (s, k) -> handle_b s k
-  | C (s, k) -> handle_b s k
+  | C (s, k) -> handle_c s k
