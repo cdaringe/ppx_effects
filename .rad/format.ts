0a8af8a378369c12d9b73df1fcf1800c7835aa82
cdaringe <cdaringe@gmail.com>
2021-10-14 18:33:26 -0700

    fix: ci cmds

--- .rad/format.ts
+++ .rad/format.ts
@@ -9,3 +9,13 @@ export const format: Task = {
     await Promise.all(cmds.map((cmd) => sh(cmd)));
   },
 };
+
+export const formatCheck: Task = {
+  async fn({ sh }) {
+    const cmds = [
+      `deno fmt --check rad.ts ui/src .rad`,
+      `opam exec -- dune build @fmt`,
+    ];
+    await Promise.all(cmds.map((cmd) => sh(cmd)));
+  },
+};
--- rad.ts
+++ rad.ts
@@ -2,7 +2,7 @@ import type { Task, Tasks } from "./.rad/common.ts";
 import { tasks as dbTasks } from "./.rad/db.ts";
 import { tasks as opamTasks } from "./.rad/opam.ts";
 import { deploy } from "./.rad/deploy.ts";
-import { format } from "./.rad/format.ts";
+import { format, formatCheck } from "./.rad/format.ts";
 
 const build = `dune build`;
 const duneExec = "opam exec -- dune exec";
@@ -30,7 +30,7 @@ const test: Task = {
 export const tasks: Tasks = {
   ...{ b: build, build },
   ...{ startAgent, sa: startAgent },
-  ...{ format, f: format },
+  ...{ format, f: format, fc: formatCheck, formatCheck },
   ...opamTasks,
   ...dbTasks,
   deploy,
